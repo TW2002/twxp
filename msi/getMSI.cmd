d7bfbff83f061cf52321360f4f5e2eef48a6f31f
MicroBlaster <dmc73120@gmail.com>
2022-12-20 04:51:38 -0600

    Updated MSIs

--- msi/getMSI.cmd
+++ msi/getMSI.cmd
@@ -1,6 +1,6 @@
 del *.msi /y
 
-copy ..\Source\Installer\TWXP27\bin\Release\TWXP27.msi TWXP27-Pre1
-copy ..\Source\Installer\TWXS27\bin\Release\TWXS27.msi TWXS27-Pre1
-copy ..\Source\Installer\MOM48\bin\Release\MOM48.msi Mombot47-Pre1
-copy ..\Source\Installer\ZED25\bin\Release\ZED25.msi Zedbot19-Pre1
+copy ..\Source\Installer\TWXP27\bin\Release\TWXP27.msi TWXP27-Pre1.msi
+copy ..\Source\Installer\TWXS27\bin\Release\TWXS27.msi TWXS27-Pre1.msi
+copy ..\Source\Installer\MOM48\bin\Release\MOM48.msi Mombot47-Pre1.msi
+copy ..\Source\Installer\ZED25\bin\Release\Zed250.msi Zedbot220-Pre1.msi
